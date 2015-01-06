99775feeb36ef7bc884aeaaa26cbb50199511dcd
Emil Lerch <emil@lerch.org>
2015-01-06 14:53:27 -0800

    update assemblyinfo

--- source/SamlPlugin/Properties/AssemblyInfo.cs
+++ source/SamlPlugin/Properties/AssemblyInfo.cs
@@ -19,12 +19,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("WsFederationPlugin")]
+[assembly: AssemblyTitle("SamlPlugin")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WsFederationPlugin")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyProduct("SamlPlugin")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
